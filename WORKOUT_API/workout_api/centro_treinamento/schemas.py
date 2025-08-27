79caffc6be0dee801ccc4244f289433da8e5dc47
Eleilton Santos <ilton.sa@outlook.com>
2025-08-27 20:57:54 -0300

    Atualizando projetos

--- WORKOUT_API/workout_api/centro_treinamento/schemas.py
+++
@@ -1,6 +0,0 @@
-from typing import Annotated
-from pydantic import Field
-from workout_api.contrib.schemas import BaseSchema
-
-class CentroTreinamento(BaseSchema):
-    nome: Annotated[str, Field(description="Nome do centro de treinamento", examples='Centro A', max_length=20)]
